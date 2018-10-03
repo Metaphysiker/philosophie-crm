f6154a0ad57d6e51815496f1bd7a9598420041e1
Metaphysiker <s.raess@me.com>
2018-10-03 23:43:38 +0200

    added capybara server puma

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,7 +10,7 @@ def login
   user = User.create!(:username => Faker::Internet.username, :email => Faker::Internet.email, :password => "secret")
   visit "/users/sign_in"
   fill_in "Login", :with => user.username
-  fill_in "Password", :with => "secret"
+  fill_in "Passwort", :with => "secret"
   click_button "Log in"
 
   expect(page).to have_selector(".navbar-brand", :text => user.username)
--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,13 +3,7 @@ require 'rails_helper'
 RSpec.describe "tasks", :type => :feature do
 
   before :each do
-    user = User.create!(:username => Faker::Internet.username, :email => Faker::Internet.email, :password => "secret")
-    visit "/users/sign_in"
-    fill_in "Login", :with => user.username
-    fill_in "Password", :with => "secret"
-    click_button "Log in"
-
-    expect(page).to have_selector(".navbar-brand", :text => user.username)
+    create_user_and_login
   end
 
   it "creates a task" do
@@ -21,7 +15,33 @@ RSpec.describe "tasks", :type => :feature do
     within find('#new_task') do
       fill_in "Description", :with => fake_text
     end
-    click_button "Create Task"
+    click_button "Aufgabe erstellen"
     expect(page).to have_content(fake_text)
+
   end
+
+  it "delets a task" do
+    person = Person.create(:email => Faker::Internet.email)
+    person.tasks << Task.create(description: Faker::Lorem.paragraph)
+    fake_text = Faker::Lorem.paragraph
+    visit "/people/#{person.id}"
+    click_on "Aufgaben anzeigen"
+    #find(".Energy_Blue").click
+    within find('#new_task') do
+      fill_in "Description", :with => fake_text
+    end
+    click_button "Aufgabe erstellen"
+    expect(page).to have_content(fake_text)
+
+  end
+end
+
+def create_user_and_login
+  user = User.create!(:username => Faker::Internet.username, :email => Faker::Internet.email, :password => "secret")
+  visit "/users/sign_in"
+  fill_in "Login", :with => user.username
+  fill_in "Passwort", :with => "secret"
+  click_button "Log in"
+
+  expect(page).to have_selector(".navbar-brand", :text => user.username)
 end
